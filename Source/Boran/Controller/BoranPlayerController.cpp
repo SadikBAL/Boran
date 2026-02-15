c03ea8d932ecad27af3e94f4f5d99a06a1373781
SADIK\sadik <12609812+SadikBAL@users.noreply.github.com>
2026-02-15 20:59:12 +0300

    Update BoranPlayerController.cpp

--- Source/Boran/Controller/BoranPlayerController.cpp
+++ Source/Boran/Controller/BoranPlayerController.cpp
@@ -5,7 +5,6 @@
 #include "Blueprint/AIBlueprintHelperLibrary.h"
 #include "NiagaraSystem.h"
 #include "NiagaraFunctionLibrary.h"
-#include "BoranCharacter.h"
 #include "Engine/World.h"
 #include "EnhancedInputComponent.h"
 #include "InputActionValue.h"
